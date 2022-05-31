1f276497dfd7283277d3f2cb78777d63594b3c57
Ryzen1998 <apexashwin@gmail.com>
2022-05-31 19:54:45 +0300

    added db connection and first api call

--- app.js
+++ app.js
@@ -1,18 +1,73 @@
-require('dotenv/config');
+require("dotenv/config");
 
-
-const express = require('express');
-
+const express = require("express");
 const app = express();
 const api = process.env.API_URL;
- 
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
-//initial route
-app.get(api+'/',(req,res)=>{
-    res.send('Hello World');
+//middlewares
+app.use(express.json());
+app.use(morgan("tiny"));
+
+//schema+models
+const productSchema = new mongoose.Schema({
+    name: String,
+    image:String,
+    countInStock:{
+        type:Number,
+        required:true
+    }
 });
 
-app.listen(3000,()=>{
-   
-    console.log('server is running on port http://localhost:3000'+api);
+const Product = mongoose.model("products", productSchema);
+
+//initial route
+app.get(`${api}/products`,async (req, res) => {
+  const productList =await Product.find();
+
+  if(!productList){
+      res.status(500).json({success:false
+    })
+  }
+
+  res.send(productList);
+});
+
+//
+
+app.post(`${api}/products`, (req, res) => {
+    const product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        countInStock: req.body.countInStock
+    });
+    product.save().then((createdProduct=>{
+        res.status(201).json(createdProduct);
+
+
+    })).catch((err)=>{
+        res.status(500).json({
+        error:err,
+        success:false
+    })
+})
+
+  
+    
+  });
+
+  //db connection
+mongoose.connect(process.env.CONNECTION_STRING,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName:'eshop'
+}).then(()=>{
+    console.log("connected to db");
+}).catch((err)=>{
+    console.log(err); 
+});
+
+app.listen(3000, () => {
+  console.log('server is running on port http://localhost:3000' + api);
 });
