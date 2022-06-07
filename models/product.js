6342e40f2d2fbb42ccc7a379d5b34d15fab25a41
Ryzen1998 <apexashwin@gmail.com>
2022-06-07 19:48:42 +0300

    updated and added more api routes

--- models/product.js
+++ models/product.js
@@ -1,14 +1,56 @@
+const { type } = require("express/lib/response");
 const mongoose = require("mongoose");
+const { stringify } = require("nodemon/lib/utils");
 
 
 
 //schema+models
 const productSchema = new mongoose.Schema({
-    name: String,
-    image:String,
+    name: {
+        type: String,
+        required: true,
+    },
+    description:{
+        type: String,
+        required: true,
+    },
+    richDescription:{
+        type: String,
+        default: "",
+       
+
+    },
+    image:{
+        type:String,
+        default:''
+
+
+    },
+    images:[{
+        type:String
+    }],
+    brand:{
+        type:String,
+        default:''
+
+    },
+    price:{
+        type:Number,
+        default:0
+
+    },
+    category:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Category',
+        required:true
+
+    },
     countInStock:{
         type:Number,
-        required:true
+        required:true,
+        min:0,
+        max:255
+
     }
 });
 
