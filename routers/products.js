f13b6ea9c11874a09e431e2feaa67f6c8a5208db
Ryzen1998 <apexashwin@gmail.com>
2022-06-13 16:08:20 +0300

    added more post and put api methods

--- routers/products.js
+++ routers/products.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { Category } = require('../models/category');
 const router = express.Router();
 
 //models import
@@ -7,7 +8,7 @@ const {Product} =require('../models/product');
 
 //initial route
 router.get(`/`,async (req, res) => {
-    const productList =await Product.find();
+    const productList =await Product.find().populate('category');
   
     if(!productList){
         res.status(500).json({success:false
@@ -18,24 +19,76 @@ router.get(`/`,async (req, res) => {
   });
   
   //
+
+  router.get(`/:id`,async (req, res) => {
+    const product =await Product.findById(req.params.id).populate('category');
   
-  router.post(`/`, (req, res) => {
+    if(!product){
+        res.status(500).json({success:false
+      })
+    }
+  
+    res.send(product);
+  });
+  
+  
+  router.post(`/`,async (req, res) => {
+
+    const category = await Category.findById(req.body.category);
+
+    if(!category) return res.status(400).send('Invalid category.');
       const product = new Product({
           name: req.body.name,
-          image: req.body.image,
-          countInStock: req.body.countInStock
+          description: req.body.description,
+          richDescription: req.body.richDescription,
+          image:req.body.image,
+          brand:req.body.brand,
+          price:req.body.price,
+          category:req.body.category,
+          countInStock: req.body.countInStock,
+          rating: req.body.rating,
+          numReviews: req.body.numReviews,
+          isFeatured: req.body.isFeatured,
       });
-      product.save().then((createdProduct=>{
-          res.status(201).json(createdProduct);
-  
-  
-      })).catch((err)=>{
-          res.status(500).json({
-          error:err,
-          success:false
-      })
-  })
+
+      product = await product.save();
+      if(!product)
+      return res.status(500).send('The product cannot be created');
+
+      res.send(product);
    });
+
+
+   
+
+   router.put('/:id',async (req,res)=>{
+    const category = await Category.findById(req.body.category);
+    if(!category) return res.status(400).send('Invalid category.');
+
+    const product =await Product.findByIdAndUpdate(req.params.id,{
+        name: req.body.name,
+        description: req.body.description,
+        richDescription: req.body.richDescription,
+        image:req.body.image,
+        brand:req.body.brand,
+        price:req.body.price,
+        category:req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured,
+    },{new:true});
+
+    if(!product){
+        return res.status(400).send('The product cannot be updated.');
+    }
+    else{
+        res.send(product);
+    }
+    
+
+});
+
 
 
    module.exports=router;
