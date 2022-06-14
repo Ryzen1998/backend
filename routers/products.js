1f4267c24e0664ac9afabb263eae2fe3ccd17621
Ryzen1998 <apexashwin@gmail.com>
2022-06-14 15:15:42 +0300

    new api routes

--- routers/products.js
+++ routers/products.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { Category } = require('../models/category');
 const router = express.Router();
+const mongoose = require('mongoose');
 
 //models import
 const {Product} =require('../models/product');
 
 
-//initial route
+
 router.get(`/`,async (req, res) => {
     const productList =await Product.find().populate('category');
   
@@ -29,6 +30,35 @@ router.get(`/`,async (req, res) => {
     }
   
     res.send(product);
+  });
+
+  router.get(`/get/count`,async (req, res) => {
+    const productCount =await Product.countDocuments();
+  
+    if(!productCount){
+        res.status(500).json({success:false
+      })
+    }
+    
+  
+    res.send({
+        count: productCount
+    });
+  });
+
+  router.get(`/get/featured/:count`,async (req, res) => {
+    const count = req.params.count?req.params.count:0;
+    const products =await Product.find({isFeatured:true}).limit(+count);
+  
+    if(!products){
+        res.status(500).json({success:false
+      })
+    }
+    
+  
+    res.send({
+        featured: products
+    });
   });
   
   
@@ -59,9 +89,13 @@ router.get(`/`,async (req, res) => {
    });
 
 
-   
+
 
    router.put('/:id',async (req,res)=>{
+   if(!mongoose.isValidObjectId(req.params.id)){
+    res.status(400).send('Invalid id.')
+
+   };
     const category = await Category.findById(req.body.category);
     if(!category) return res.status(400).send('Invalid category.');
 
@@ -88,6 +122,24 @@ router.get(`/`,async (req, res) => {
     
 
 });
+
+router.delete('/:id',(req,res)=>{
+
+    Product.findByIdAndRemove(req.params.id).then(product=>{
+        if(product){
+            return res.status(200).json({success:true,message:'product deleted successfully'});
+        }
+        else
+        {
+            return res.status(404).json({success:false,message:'product not found'});
+        }
+    }).catch(err=>{
+        return res.status(400).json({success:false,message:'error occured'});
+    });
+
+
+});
+
 
 
 
